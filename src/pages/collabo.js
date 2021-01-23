9cd567e86864a945c87e6b6d5d93672240fa6c4b
afukano <a.fukano@modulat.com>
2021-01-24 00:33:00 +0900

    コラボに時間表記追加

--- src/pages/collabo.js
+++ src/pages/collabo.js
@@ -8,12 +8,12 @@ const SecondPage = () => (
   <Layout>
     <SEO title="コラボ" />
     <h2>【告知あり】なにかとても楽しくなる予感の放送をします【すごいと思う】 / 事務員G</h2>
-    <p>情熱大陸 / かてぃん</p>
+    <p>情熱大陸 / かてぃん(1:26:00～)</p>
     <p>
       <iframe width="501" height="282" src="https://www.youtube.com/embed/GOs0KyBD47A?t=5160" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </p>
     <h2>【ゲストたくさん】スタジオからグランドピアノでお送りします！ / 事務員G</h2>
-    <p>シロナ戦(ポケモンDP) / 瀬戸一王</p>
+    <p>シロナ戦(ポケモンDP) / 瀬戸一王(1:32:52～)</p>
     <p>
       <iframe width="501" height="282" src="https://www.youtube.com/embed/YVj3qWC2Hn8?t=5572" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </p>
